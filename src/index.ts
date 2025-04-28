87f10c054ca6d2c73e3a5d9f757ebc1c877f3b38
Maximilien <maximilien.philippe@protonmail.com>
2025-04-28 10:32:29 +0200

    feat(02-D): init apollo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import "dotenv/config";
 import { dataSource } from "./services/client.service";
+import { buildSchema } from "type-graphql";
+import PaysResolver from "./resolvers/pays.resolver";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 (async () => {
   await dataSource.initialize();
-})();
 
-console.log("Hello world");
+  const schema = await buildSchema({
+    resolvers: [PaysResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`Server listen at : ${url}`);
+})();
