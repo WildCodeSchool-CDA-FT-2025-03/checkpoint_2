3b08208277cf4274a58081097463c501dcaf0ed1
Maximilien <maximilien.philippe@protonmail.com>
2025-04-28 10:05:55 +0200

    feat(03-B): add db.sqlite in gitignore and constructor in pays entity

--- src/entities/pays.entity.ts
+++ src/entities/pays.entity.ts
@@ -1,7 +1,18 @@
+import { Field, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class PaysEntity extends BaseEntity {
+  constructor() {
+    super();
+    this.id = 0;
+    this.name = "";
+    this.code = "";
+    this.flag = "";
+  }
+
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
