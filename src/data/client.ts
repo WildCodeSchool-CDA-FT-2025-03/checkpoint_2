cfbea057a132e43c61a13d980733c6c7254e24b3
rodolphe <rodolphe.tournier@gmail.com>
2025-04-28 10:53:59 +0200

    feat: add data client

---
+++ src/data/client.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+import { Country } from "../entities/country.entity";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "sqlite", // type de ma db
+  database: "db.sqlite", // nom de ma db
+  entities: [Country], // les entités de ma db
+  synchronize: true, // synchroniser les entités avec la db
+});
