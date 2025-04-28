00e697414137711ec9f90ac6d89cc048500d9310
Flam64 <celio.mozes.rocha@gmail.com>
2025-04-28 13:43:55 +0200

    S2-US@E - Creation de la mutation pour enregistrer un pays dans la base de données

--- src/pays/pays.entity.ts
+++ src/pays/pays.entity.ts
@@ -1,5 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { Field, ID, ObjectType } from "type-graphql";
+import { Field, ID, InputType, ObjectType } from "type-graphql";
+import { Length } from "class-validator";
 
 @ObjectType()
 @Entity("pays")
@@ -18,5 +19,20 @@ export class Pays extends BaseEntity {
 
   @Field()
   @Column()
+  flag: string;
+}
+
+@InputType()
+export class PaysInput {
+  @Field()
+  @Length(30)
+  name: string;
+
+  @Field()
+  @Length(2)
+  code: string;
+
+  @Field()
+  @Length(30)
   flag: string;
 }
--- src/pays/pays.resolver.ts
+++ src/pays/pays.resolver.ts
@@ -1,10 +1,39 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
-import { Pays } from "../pays/pays.entity";
+import { Resolver, Query, Mutation, Arg, Int } from "type-graphql";
+import { Pays, PaysInput } from "../pays/pays.entity";
+import { validate } from "graphql";
+import { rmSync } from "fs";
 
 @Resolver(Pays)
 export class PaysResolver {
   @Query(() => [Pays])
-  async getHello(): Promise<Pays[]> {
+  async getPays(): Promise<Pays[]> {
     return await Pays.find();
+  }
+
+  @Mutation(() => Boolean)
+  async createPays(@Arg("data") data: PaysInput) {
+    try {
+      const { name, code, flag } = data;
+
+      // creation du pays
+      const newPays: Pays = new Pays();
+      newPays.name = name;
+      newPays.code = code;
+      newPays.flag = flag;
+
+      // Validation des données
+
+      //const error = await validate(newPays)
+
+      const result = await newPays.save();
+
+      if (result.id) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      throw new Error();
+    }
   }
 }
