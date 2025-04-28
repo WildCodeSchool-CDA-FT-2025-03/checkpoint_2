63750233a9750f96671d44f1a47dff4fe3120c10
Dilgo-dev <dilgopierre@gmail.com>
2025-04-28 10:01:37 +0200

    chore(entity/country): Add class-validator to country entity

--- src/entities/country.entity.ts
+++ src/entities/country.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 import { Field, ID, ObjectType } from "type-graphql";
+import { Length, IsNotEmpty, Matches } from "class-validator";
 
 @ObjectType()
 @Entity()
@@ -10,14 +11,20 @@ export class Country extends BaseEntity {
 
   @Field(() => String)
   @Column({ type: "varchar" })
+  @IsNotEmpty({ message: "Le nom ne peut pas être vide" })
+  @Length(2, 100, { message: "Le nom doit contenir entre 2 et 100 caractères" })
   name: string;
 
   @Field(() => String)
   @Column({ type: "varchar" })
+  @IsNotEmpty({ message: "Le code ne peut pas être vide" })
+  @Length(2, 3, { message: "Le code doit contenir 2 ou 3 caractères" })
+  @Matches(/^[A-Z]{2,3}$/, { message: "Le code doit être composé de 2 ou 3 lettres majuscules" })
   code: string;
 
   @Field(() => String)
   @Column({ type: "varchar" })
+  @IsNotEmpty({ message: "Le drapeau ne peut pas être vide" })
   flag: string;
 
   @Field(() => Date)
