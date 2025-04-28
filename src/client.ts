7f999bcefd024d082c37abb14c4a989084a8b022
Julien RICHARD <julien.y.richard@gmail.com>
2025-04-28 15:54:46 +0200

    Step 04, Docker

--- src/client.ts
+++ src/client.ts
@@ -3,11 +3,28 @@ import { Country } from "./countries/country.entity";
 import "dotenv/config";
 import { Continent } from "./continents/continents.entity";
 
+// const dataSource = new DataSource({
+//   type: "sqlite",
+//   database: process.env.DB_FILE || "./db.sqlite",
+//   entities: [Country, Continent],
+//   synchronize: true,
+// });
+
 const dataSource = new DataSource({
-  type: "sqlite",
-  database: process.env.DB_FILE || "./db.sqlite",
-  entities: [Country, Continent],
+  type: "postgres",
+  host: "postgres",
+  port: 5432,
+  username: process.env.POSTGRES_USER as string,
+  password: process.env.POSTGRES_PASSWORD as string,
+  database: process.env.POSTGRES_DB as string,
   synchronize: true,
+  entities: [Continent, Country],
 });
 
 export default dataSource;
+
+/**
+ * - POSTGRES_PASSWORD=password
+      - POSTGRES_USER=julien
+      - POSTGRES_DB=checkpoint
+ */
